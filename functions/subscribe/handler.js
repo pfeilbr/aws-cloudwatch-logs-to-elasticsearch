aa0c872c20a5bfeea182e94ed3f38149bd2db230
Brian <brian.pfeil@gmail.com>
2018-03-06 07:35:18 -0500

    init

---
+++ functions/subscribe/handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const co             = require('co');
+const Promise        = require('bluebird');
+const AWS            = require('aws-sdk');
+const cloudWatchLogs = Promise.promisifyAll(new AWS.CloudWatchLogs());
+const destArn        = process.env.DEST_FUNC;
+const destFuncName   = destArn.split(":").reverse()[0];
+
+let subscribe = function* (logGroupName) {
+  let options = {
+    destinationArn : destArn,
+    logGroupName   : logGroupName,
+    filterName     : 'ship-logs',
+    filterPattern  : `[timestamp=*Z, request_id="*-*", event]`
+  };
+
+  yield cloudWatchLogs.putSubscriptionFilterAsync(options);
+};
+
+module.exports.handler = co.wrap(function* (event, context, callback) {
+  console.log(JSON.stringify(event));
+  
+  // eg. /aws/lambda/logging-demo-dev-api
+  let logGroupName = event.detail.requestParameters.logGroupName;
+  console.log(`log group: ${logGroupName}`);
+
+  if (logGroupName === `/aws/lambda/${destFuncName}`) {
+    console.log("ignoring the log group for the ship-logs function to avoid invocation loop!");
+    callback(null, 'ignored');
+  } else {
+    yield subscribe(logGroupName);
+    console.log(`subscribed [${logGroupName}] to [${destArn}]`);
+  
+    callback(null, 'ok');
+  }
+});
